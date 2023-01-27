444e52d0023791a653580c6ca1045f778e027020
AbdellahElmrini <abdellah.el-mrini@polytechnique.edu>
2023-01-27 02:55:59 +0100

    Added some classifiers + evaluation code

---
+++ src/classifiers/models.py
@@ -0,0 +1,82 @@
+import pathlib
+import typing as t
+
+from matplotlib import pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+
+from src.utils.pathtools import project
+from src.utils.logging import logger
+from src.utils.datasets import tiny_imagenet, Datasets_tiny_imagenet
+
+
+"""
+    Models with frozen backbones
+"""
+
+class DenseNet(nn.Module):
+    def __init__(self):
+        super(DenseNet, self).__init__()
+        self.densenet = torchvision.models.densenet201(weights=torchvision.models.DenseNet201_Weights.DEFAULT)
+        for param in self.densenet.parameters():
+            param.requires_grad = False
+        self.densenet.classifier = nn.Sequential(
+            nn.Linear(1920, 512),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(512, 200),
+            nn.LogSoftmax(dim=1)
+        )
+    
+    def forward(self, x):
+        return self.densenet(x)
+    
+    def get_features(self, x):
+        return self.densenet.features(x)
+    
+        
+class VGG(nn.Module):
+    def __init__(self):
+        super(VGG, self).__init__()
+        self.vgg = torchvision.models.vgg16(weights=torchvision.models.VGG16_Weights.DEFAULT)
+        for param in self.vgg.parameters():
+            param.requires_grad = False
+        self.vgg.classifier = nn.Sequential(
+            nn.Linear(25088, 4096),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(4096, 4096),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(4096, 200),
+            nn.LogSoftmax(dim=1)
+        )
+    
+    def forward(self, x):
+        return self.vgg(x)
+    
+    def get_features(self, x):
+        return self.vgg.features(x)
+
+class ResNet(nn.Module):
+    def __init__(self):
+        super(ResNet, self).__init__()
+        self.resnet = torchvision.models.resnet50(weights=torchvision.models.ResNet50_Weights.DEFAULT)
+        for param in self.resnet.parameters():
+            param.requires_grad = False
+        self.resnet.fc = nn.Sequential(
+            nn.Linear(2048, 512),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(512, 200),
+            nn.LogSoftmax(dim=1)
+        )
+    
+    def forward(self, x):
+        return self.resnet(x)
+    
+    def get_features(self, x):
+        return self.resnet.conv1(x)
